d0d61e38befad300c7faa67e59d99f6c61ca79a8
zhangning <zhangning@boe.com.cn>
2020-07-22 13:31:03 +0800

    init

--- id_generator.go
+++ id_generator.go
@@ -15,25 +15,31 @@ import (
 
 /*
   return eg: 4725f5ae6a350b1c45687c9934456e6f
- */
-func GUID() string {
+*/
+func GUID() (string, error) {
 	b := make([]byte, 48)
 	_, err := io.ReadFull(rand.Reader, b)
-	CheckAndPrintError("Guid generate io rand reader failed", err)
-	guid := Md5Signer(base64.URLEncoding.EncodeToString(b))
-	return guid
+	if err != nil {
+		return "", err
+	}
+	guid := Md5(base64.URLEncoding.EncodeToString(b))
+	return guid, nil
 }
 
 /*
  return eg: e7486845-9f24-c3d8-0db1-fe61e25c88a2
- */
-func UUID() string {
-	str := NewString(GUID())
+*/
+func UUID() (string, error) {
+	guid, err := GUID()
+	if err != nil {
+		return "", err
+	}
+	str := NewString(guid)
 	builder := NewStringBuilder()
 	builder.Append(str.Substring(0, 8).ToString())
 	builder.Append(HLINE).Append(str.Substring(8, 12).ToString())
 	builder.Append(HLINE).Append(str.Substring(12, 16).ToString())
 	builder.Append(HLINE).Append(str.Substring(16, 20).ToString())
 	builder.Append(HLINE).Append(str.SubstringBegin(20).ToString())
-	return builder.ToString()
+	return builder.ToString(), nil
 }
