8cd77f7e7e07cde11c1eac4dc51d8a15fcfc83df
zhangning <zhangning@boe.com.cn>
2020-08-20 18:39:52 +0800

    modify templte

--- template.go
+++ template.go
@@ -2,17 +2,24 @@ package go_utils
 
 import (
 	"bytes"
+	"errors"
 	"html/template"
 )
 
 // 转换模板
-func ParseTemplate(data interface{}, templateFile string) (string, error) {
+func ParseTemplate(name string, funcMap template.FuncMap, data interface{}, templateFile string) (string, error) {
 
-	t, err := template.ParseFiles(templateFile)
+	if len(name) == 0 {
+		return "", errors.New("Name cannot be empty")
+	}
+	t := template.New(name)
+	if funcMap != nil {
+		t = t.Funcs(funcMap)
+	}
+	t, err := t.ParseFiles(templateFile)
 	if err != nil {
 		return "", err
 	}
-
 	buf := new(bytes.Buffer)
 	if err = t.Execute(buf, data); err != nil {
 		return "", err
