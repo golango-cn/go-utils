1cd85a38c868490cb71dc6c25550f6a976b44397
zhangning <zhangning@boe.com.cn>
2020-11-19 16:46:12 +0800

    fix

--- json.go
+++ json.go
@@ -1,6 +1,9 @@
 package go_utils
 
-import "strings"
+import (
+	"encoding/json"
+	"strings"
+)
 
 // 移除Json字段
 func RemoveJsonField(fields []string, mp map[string]interface{}) map[string]interface{} {
@@ -31,4 +34,25 @@ func RemoveJsonField(fields []string, mp map[string]interface{}) map[string]inte
 
 	return mp
 
+}
+
+func RemoveJsonStringField(fields []string, str string) string {
+
+	if len(fields) == 0 {
+		return str
+	}
+	if len(str) == 0 {
+		return str
+	}
+
+	var mp map[string]interface{}
+	err := json.Unmarshal([]byte(str), &mp)
+	if err != nil {
+		return str
+	}
+
+	mp = RemoveJsonField(fields, mp)
+	b, _ := json.Marshal(mp)
+
+	return string(b)
 }
