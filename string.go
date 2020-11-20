d32efa4eaaf4262d173b1fbf252a4137ccba927f
zhangning <zhangning@boe.com.cn>
2020-11-20 11:18:45 +0800

    init

--- string.go
+++ string.go
@@ -8,6 +8,7 @@ package go_utils
 */
 
 import (
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -25,7 +26,7 @@ import (
 		13990521234
 		14
 		1314520
- */
+*/
 type String struct {
 	value string
 }
@@ -66,43 +67,43 @@ func (str *String) Clear() *String {
 
 /*
  "123" -> 3
- */
+*/
 func (str *String) Len() int {
 	return len(str.value)
 }
 
 /*
 	"123xxxbbb5990" -> "123x" = true
- */
+*/
 func (str *String) StartsWith(s string) bool {
 	return str.SubstringEnd(len(s)).ToString() == s
 }
 
 /*
 	"123xxxbbb5990" -> "5990" = true
- */
+*/
 func (str *String) EndsWith(s string) bool {
-	return str.SubstringBegin(str.Len() - len(s)).ToString() == s
+	return str.SubstringBegin(str.Len()-len(s)).ToString() == s
 }
 
 /*
   " 123 " -> "123"
   " 1 23 " -> "1 23"
- */
+*/
 func (str *String) Trim() *String {
 	return NewString(strings.Trim(str.value, SPACE))
 }
 
 /*
   "%111%abc%987%" -> ("%", "$") -> "$111$abc$987$"
- */
+*/
 func (str *String) Replace(old, new string) *String {
 	return NewString(strings.Replace(str.value, old, new, -1))
 }
 
 /*
 	"abc" -> 1 -> "ac"
- */
+*/
 func (str *String) Remove(index int) *String {
 	strTmp := NewStringBuilder().Append(str.SubstringEnd(index).ToString()).Append(str.SubstringBegin(index + 1).ToString()).ToString()
 	return NewString(strTmp)
@@ -110,7 +111,7 @@ func (str *String) Remove(index int) *String {
 
 /*
 	"abc" -> "ab"
- */
+*/
 func (str *String) RemoveLast() *String {
 	return str.Substring(0, str.Len()-1)
 }
@@ -139,21 +140,21 @@ func (str *String) Contains(chars string) bool {
 
 /*
   abcdef -> b = 1
- */
+*/
 func (str *String) LastIndex(chars string) int {
 	return strings.LastIndex(str.value, chars)
 }
 
 /*
   abcdef -> e = 4
- */
+*/
 func (str *String) Index(chars string) int {
 	return strings.Index(str.value, chars)
 }
 
 /*
    "12345" -> 12345
- */
+*/
 func (str *String) ToInt() (int, error) {
 	return strconv.Atoi(str.value)
 }
@@ -170,16 +171,16 @@ func (str *String) ToFloat() (float64, error) {
   str := NewString("abcde")
   str.Substring(0, 2)
   return: "ab"
- */
+*/
 func (str *String) Substring(beginIndex, endIndex int) *String {
-	return NewString(str.value[beginIndex: endIndex])
+	return NewString(str.value[beginIndex:endIndex])
 }
 
 /*
   str := NewString("abcde")
   str.SubstringBegin(2)
   return: "cde"
- */
+*/
 func (str *String) SubstringBegin(beginIndex int) *String {
 	return str.Substring(beginIndex, str.Len())
 }
@@ -188,7 +189,7 @@ func (str *String) SubstringBegin(beginIndex int) *String {
   str := NewString("abcde")
   str.SubstringEnd(3)
   return: "abc"
- */
+*/
 func (str *String) SubstringEnd(endIndex int) *String {
 	return str.Substring(0, endIndex)
 }
@@ -220,21 +221,21 @@ func (str *String) AppendFloat64(f float64) *String {
 
 /*
   "460364431014955c2483ec91230e5435" -> [4 6 0 3 6 4 4 3 1 0 1 4 9 5 5 c 2 4 8 3 e c 9 1 2 3 0 e 5 4 3 5]
- */
+*/
 func (str *String) ToArray() []string {
 	return strings.Split(str.value, "")
 }
 
 /*
   "aaa" -> "AAA"
- */
+*/
 func (str *String) ToLower() *String {
 	return NewString(strings.ToLower(str.value))
 }
 
 /*
   "BBB" -> "bbb"
- */
+*/
 func (str *String) ToUpper() *String {
 	return NewString(strings.ToUpper(str.value))
 }
@@ -242,7 +243,7 @@ func (str *String) ToUpper() *String {
 /*
   first = false: "aaa_bbb_ccc" -> "aaaBbbCcc"
   first = true: "aaa_bbb_ccc" -> "AaaBbbCcc"
- */
+*/
 func FirstCaseToUpper(str string, first bool) string {
 	temp := strings.Split(str, "_")
 	var upperStr string
@@ -269,7 +270,13 @@ func FirstCaseToUpper(str string, first bool) string {
 
 /*
  [9 9 8 4 2 9 1 7 - a 5 4 b - 3 3 1 6 - c d f 3 - 8 7 d 9 f b 5 7] -> "99842917-a54b-3316-cdf3-87d9fb57"
- */
+*/
 func ArrayToString(arrays []string) string {
 	return strings.Join(arrays, "")
+}
+
+// 移除base64编码
+func RemoveBase64(str string) string {
+	r := regexp.MustCompile(`[a-zA-Z0-9+/]{200,}`)
+	return r.ReplaceAllString(str, "")
 }
